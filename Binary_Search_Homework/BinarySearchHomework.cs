ec7a40a4a3e105c814aa7377a5b783b95fdc29ca
mahmoud <mahmoudmasa81@gmail.com>
2023-10-26 13:42:21 +0300

    submit homework

--- Binary_Search_Homework/BinarySearchHomework.cs
+++ Binary_Search_Homework/BinarySearchHomework.cs
@@ -13,25 +13,95 @@ namespace Binary_Search_Homework // don't edit this line!!!
                                                                                 // لكن لاتقوم بحذفهم أو التعديل عليهم هنا تحت اي ظرف!!!
                                                                                 // من الممكن تمرير قيمة 0 لتجاهلهم
         {
+            int left = 0;
+            int right = arr.Length - 1;
+
+            while (left <= right)
+            {
+
+                int mid1 = left + (right - left) / 3;
+                int mid2 = right - (right - left) / 3;
+
+                if (arr[mid1] == key)
+                {
+                    return mid1;
+                }
+
+                if (arr[mid2] == key)
+                {
+                    return mid2;
+                }
+
+                if (key < arr[mid1])
+                {
+                    right = mid1 - 1;
+                }
+                else if (key > arr[mid2])
+                {
+                    left = mid2 + 1;
+                }
+                else
+                {
+                    left = mid1 + 1;
+                    right = mid2 - 1;
+                }
+            }
             //TODO: write code for the ternary search algorithm and return the index of the element
             return -1;
         }
 
-        public static int BinarySearchForCalculatingRepeated
-            (int[] arr, int key, bool is_first, int start, int end) // don't edit this line!!!
+        public static int BinarySearchForCalculatingRepeated(int[] arr, int key, bool is_first, int start, int end) // don't edit this line!!!
                                                                     // يمكنك تجاهل برمترات البداية والنهاية إذا لا تريد استخدام الطريقة العودية 
                                                                     // لكن لاتقوم بحذفهم أو التعديل عليهم هنا تحت اي ظرف!!!
                                                                     // من الممكن تمرير قيمة 0 لتجاهلهم
         {
+            int left = 0;
+            int right = arr.Length - 1;
+            int result = -1;
+
+            while (left <= right)
+            {
+                int mid = left + (right - left) / 2;
+
+                if (arr[mid] == key)
+                {
+                    result = mid;
+                    right = mid - 1;
+                }
+                else if (arr[mid] < key)
+                {
+                    left = mid + 1;
+                }
+                else
+                {
+                    right = mid - 1;
+                }
+            }
+
             //TODO: this methods is for getting the first accurence of the key and the last accurance
-            return -1;
+            return result;
         }
 
         public static int GetRepeatCount(int[] arr, int key) // don't edit this line!!!
         {
+            int start = BinarySearchForCalculatingRepeated(arr, key,true,0,0);
+
+            if (start == -1)
+            {
+                return -1; 
+            }
+
+            int end = start;
+
+            
+            while (end + 1 < arr.Length && arr[end + 1] == key)
+            {
+                end++;
+            }
+
             //TODO: write code to calculate the repeat count of a spacific element
             // make sure to use the previous method in this method
-            return -1;
+            return end - start + 1;
         }
     }
 }
